f8b032805b1e20e1288e5354ae735adc45cc91c6
Hidde Jansen <webmaster@electricnation.nl>
2011-03-16 16:59:03 +0100

    Mostly working

--- Manga.xcodeproj/ReadMangaViewController.h
+++ Manga.xcodeproj/ReadMangaViewController.h
@@ -7,24 +7,37 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "MangaDetailView.h"
 
 @interface ReadMangaViewController : UIViewController {
+    NSThread * zipThread;
     NSThread * readmeThread;
     
+    BOOL readMePanelActive;
     NSString * mangaName;
     NSString * readmeString;
+    IBOutlet UIProgressView *zipProgressView;
     IBOutlet UILabel *titleLabel;
-    IBOutlet UILabel *infoBox;
-    IBOutlet UIButton *readMeButton;
+    IBOutlet UIButton *ReadMangaButton;
+    IBOutlet UILabel *loadingLabel;
+    IBOutlet MangaDetailView *mainDetailView;
+    IBOutlet UIView *OptionView;
+    IBOutlet UIView *ReadMeView;
+    IBOutlet UITextView *readMeDetailView;
 }
 
+@property (nonatomic, retain) IBOutlet UILabel *loadingLabel;
 @property (nonatomic, retain) IBOutlet UILabel *titleLabel;
-@property (nonatomic, retain) IBOutlet UILabel *infoBox;
-@property (nonatomic, retain) IBOutlet UIButton *readMeButton;
+@property (nonatomic, retain) IBOutlet UIButton *ReadMangaButton;
+@property (nonatomic, retain) IBOutlet UITextView *readMeDetailView;
+@property (nonatomic, retain) IBOutlet UIProgressView *zipProgressView;
 
 - (void)setMangaName:(NSString*)newName;
 - (void)scanZipForTextFile:(NSString*)zipName;
-- (IBAction)showReadMe:(id)sender;
+- (void)extractImagesFromZip:(NSString*)zipName;
+- (void)createFileWithData:(NSData*)data atPath:(NSString*)filePath;
+- (void)createDirWithTargetPathComponents:(NSArray*)targetPathComponents withMangaDir:(NSString*)mangaDir;
+- (IBAction)showReadMe;
+- (IBAction)dismissReadMePanel;
 
 @end
--- Manga.xcodeproj/ReadMangaViewController.m
+++ Manga.xcodeproj/ReadMangaViewController.m
@@ -6,8 +6,8 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h> 
 #import "ReadMangaViewController.h"
-#import "ReadMeViewController.h"
 #import "ZipFile.h"
 #import "ZipException.h"
 #import "FileInZipInfo.h"
@@ -16,9 +16,11 @@
 
 @implementation ReadMangaViewController
 
+@synthesize loadingLabel;
 @synthesize titleLabel;
-@synthesize infoBox;
-@synthesize readMeButton;
+@synthesize ReadMangaButton;
+@synthesize readMeDetailView;
+@synthesize zipProgressView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -63,6 +65,16 @@
     // Do any additional setup after loading the view from its nib.
     [[self navigationController] setTitle:mangaName];
     [titleLabel setText:[[mangaName lastPathComponent] stringByDeletingPathExtension]];
+    
+    mainDetailView.layer.shadowColor = [[UIColor blackColor] CGColor];
+    mainDetailView.layer.shadowOffset = CGSizeMake(0.0, 0.0);
+    mainDetailView.layer.shadowOpacity = 1.0;
+    mainDetailView.layer.shadowRadius = 10;
+    
+    ReadMeView.layer.shadowColor = [[UIColor blackColor] CGColor];
+    ReadMeView.layer.shadowOffset = CGSizeMake(0.0, 0.0);
+    ReadMeView.layer.shadowOpacity = 1.0;
+    ReadMeView.layer.shadowRadius = 10;
 }
 
 - (void)scanZipForTextFile:(NSString*)zipName{
@@ -86,28 +98,192 @@
             int bytesRead= [read readDataWithBuffer:data];
             [read finishedReading];
             readmeString = [[[NSString alloc] initWithBytes:[data bytes] length:bytesRead encoding:NSUTF8StringEncoding] autorelease];
-            [infoBox setText:readmeString];
-            [readMeButton setEnabled:YES];
         }
     }
     [unzipFile close];
     [unzipFile release];
     
+    NSArray * paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString * cacheDirectory = [paths objectAtIndex:0];
+    NSString * mangaDirectory = [cacheDirectory stringByAppendingPathComponent:[mangaName stringByDeletingPathExtension]];
+    
+    NSFileManager * filemanager = [NSFileManager defaultManager];
+    
+    //Dir does not exist: create it
+    BOOL isDir;
+    if(![filemanager fileExistsAtPath:mangaDirectory isDirectory:&isDir] || !isDir)
+    {
+        zipThread= [[NSThread alloc] initWithTarget:self selector:@selector(extractImagesFromZip:) object:zipName];
+        [zipThread start];
+    }
 	[pool drain];
 }
 
-- (IBAction)showReadMe:(id)sender {
-    ReadMeViewController *readMeView = [[ReadMeViewController alloc] init];
-    readMeView.readMeTextView.text = readmeString;
-    [self.navigationController pushViewController:readMeView animated:YES];
-    [readMeView release];
+- (void)extractImagesFromZip:(NSString*)zipName {
+    NSAutoreleasePool *pool= [[NSAutoreleasePool alloc] init];
+    
+    ZipFile *unzipFile= [[ZipFile alloc] initWithFileName:zipName mode:ZipFileModeUnzip];
+    NSArray *infos= [unzipFile listFileInZipInfos];
+    
+    int count = 0;
+    float progress = 0;
+    [zipProgressView setProgress:progress];
+    
+    NSArray * paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString * cacheDirectory = [paths objectAtIndex:0];
+    NSString * mangaCleanName = [mangaName stringByDeletingPathExtension];
+    NSString * mangaDirectory = [cacheDirectory stringByAppendingPathComponent:mangaCleanName];
+    
+    NSFileManager * filemanager = [NSFileManager defaultManager];
+    
+    //Dir does not exist: create it
+    BOOL isDir;
+    if(![filemanager fileExistsAtPath:mangaDirectory isDirectory:&isDir] || !isDir)
+    {
+        [filemanager createDirectoryAtPath:mangaDirectory withIntermediateDirectories:NO attributes:nil error:NULL];
+    }
+    
+    for (FileInZipInfo *info in infos) {
+        //NSLog(@"- %@ %@ %d (%d)", info.name, info.date, info.size, info.level);
+        
+        // Locate the file in the zip
+        [unzipFile locateFileInZip:info.name];
+        
+        
+        if([info.name hasSuffix:@".jpg"] || [info.name hasSuffix:@".JPG"] || [info.name hasSuffix:@".PNG"] || [info.name hasSuffix:@".png"] || [info.name hasSuffix:@".GIF"] || [info.name hasSuffix:@".gif"])
+        {
+            NSLog(@"%@ == image file", info.name);
+            
+            NSMutableArray * targetPathComponents = [NSMutableArray arrayWithCapacity:[[info.name pathComponents] count]];
+            [targetPathComponents addObjectsFromArray:[info.name pathComponents]];
+            [targetPathComponents removeLastObject];
+            
+            if([targetPathComponents count] >= 1)
+            {
+                [self createDirWithTargetPathComponents:targetPathComponents withMangaDir:mangaDirectory];
+            }
+            
+            // Expand the file in memory
+            ZipReadStream *read= [unzipFile readCurrentFileInZip];
+            NSMutableData *data= [[NSMutableData alloc] initWithLength:info.length];
+            int bytesRead= [read readDataWithBuffer:data];
+            [read finishedReading];
+            
+            NSString * theTargetDir = [mangaDirectory stringByAppendingPathComponent:info.name];
+            
+            [self createFileWithData:data atPath:theTargetDir];
+            
+            //Update Progress
+            count++;
+            progress = (float) count / [infos count];
+            [self performSelectorOnMainThread:@selector(loadingProgress:) withObject:[NSNumber numberWithFloat:progress] waitUntilDone:NO];
+            [zipProgressView setProgress:progress];
+            NSLog(@"Progress %f", progress);
+            
+            targetPathComponents = nil;
+        }
+    }
+    [zipProgressView setProgress:1];
+    [unzipFile close];
+    [unzipFile release];
+    
+    
+    [loadingLabel setText:@"Manga loaded"];
+    [ReadMangaButton setEnabled:YES];
+    [zipProgressView setHidden:YES];
+    
+	[pool drain];
+}
+- (void)createFileWithData:(NSData*)data atPath:(NSString*)filePath {
+    NSFileManager * filemanager = [NSFileManager defaultManager];
+
+    if(![filemanager fileExistsAtPath:filePath])
+    {
+        [filemanager createFileAtPath:filePath contents:data attributes: nil];
+    }
+    
+}
+-(void)createDirWithTargetPathComponents:(NSArray*)targetPathComponents withMangaDir:(NSString*)mangaDir{
+    
+    NSFileManager * filemanager = [NSFileManager defaultManager];
+    NSString * filePath = mangaDir;
+    
+    for (NSString * component in targetPathComponents)
+    {
+        filePath = [filePath stringByAppendingPathComponent:component];
+       
+        //Dir does not exist: create it
+        BOOL isDir = NO;
+        if(![filemanager fileExistsAtPath:filePath isDirectory:&isDir] && !isDir)
+        {
+            [filemanager createDirectoryAtPath:filePath withIntermediateDirectories:NO attributes:nil error:NULL];
+        }
+    }
+    
+}
+
+- (IBAction)toggleReadMe {
+    if (readMePanelActive == YES)
+    {
+        readMePanelActive = NO;
+        [self dismissReadMePanel];
+    }
+    else
+    {
+        readMePanelActive = YES;
+        [self showReadMe];
+        
+    }
+}
+
+- (void)showReadMe {
+    [readMeDetailView setText:readmeString];
+    
+    CGRect viewFrame = [OptionView frame];
+    viewFrame.origin.y = 480;
+    ReadMeView.frame = viewFrame;
+    
+    [UIView beginAnimations:@"animateView" context:nil];
+    [UIView setAnimationDuration:0.5];
+    
+    CGRect viewFrame2 = [OptionView frame];
+    viewFrame2.origin.y = viewFrame2.origin.y + 40;
+    
+    ReadMeView.frame = viewFrame2;
+    
+    [self.view addSubview:ReadMeView];
+    [UIView commitAnimations];
+}
+
+- (void)dismissReadMePanel {
+    [UIView beginAnimations:@"animateView" context:nil];
+    [UIView setAnimationDuration:0.5];
+    
+    CGRect viewFrame = [OptionView frame];
+    viewFrame.origin.y = 480;
+    ReadMeView.frame = viewFrame;
+    
+    [self.view addSubview:ReadMeView];
+    [UIView commitAnimations];
+}
+
+-(void)loadingProgress:(NSNumber *)nProgress{
+    float progress = [nProgress floatValue];
+    NSLog(@"Progress %f", progress);
+    zipProgressView.progress = progress;
 }
 
 - (void)viewDidUnload
 {
     [titleLabel release];
     [infoBox release];
-    [readMeButton release];
+    [OptionView release];
+    [ReadMeView release];
+    [readMeDetailView release];
+    [mainDetailView release];
+    [zipProgressView release];
+    [loadingLabel release];
+    [ReadMangaButton release];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
